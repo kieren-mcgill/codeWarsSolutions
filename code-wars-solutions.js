8e01fe4d4e1ad58eb6823b12d8146c8dafa67138
kacm79 <105650651+kacm79@users.noreply.github.com>
2022-10-21 16:18:47 +0100

    Update code-wars-solutions.js

--- code-wars-solutions.js
+++ code-wars-solutions.js
@@ -8,7 +8,22 @@ function findNeedle(haystack) {
   }
 }
 
-//The highest profit wins!
+
+//The highest profit wins (7 kyu)
 
 const minMax = (arr) => [(Math.min.apply(null, arr)), (Math.max.apply(null, arr))];
-console.log(minMax([1, 6, 10, 45]));
+
+
+//Remove the minimum(7 kyu)
+
+function removeSmallest(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  } else for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] === Math.min(...numbers)) {
+  const newList = numbers.slice(0, i).concat(numbers.slice(i+1, numbers.length));
+    return newList
+  }
+ }
+}
+
