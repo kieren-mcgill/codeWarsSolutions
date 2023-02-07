217de98eb27eabf465fd2606db5437d732c9c268
kacm79 <105650651+kacm79@users.noreply.github.com>
2023-02-07 17:35:43 +0000

    Update code-wars-solutions.js

--- code-wars-solutions.js
+++ code-wars-solutions.js
@@ -142,3 +142,10 @@ function highAndLow(numbers){
   let numArr = numbers.split(" ").map(str => +str);
   return `${Math.max(...numArr)} ${Math.min(...numArr)}`;
 }
+
+
+//Find the middle array (7kyu)
+
+function gimme (triplet) {
+return (triplet.indexOf([...triplet].sort((a, b) => a - b)[1]));
+}
