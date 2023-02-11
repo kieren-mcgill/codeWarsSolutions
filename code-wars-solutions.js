84d5cabb4d565c50f9dfcddb93525ba3d5abf7f6
kacm79 <105650651+kacm79@users.noreply.github.com>
2023-02-11 12:16:25 +0000

    Update code-wars-solutions.js

--- code-wars-solutions.js
+++ code-wars-solutions.js
@@ -149,3 +149,19 @@ function highAndLow(numbers){
 function gimme (triplet) {
 return (triplet.indexOf([...triplet].sort((a, b) => a - b)[1]));
 }
+
+//Make a function that does arithmetic!
+
+function arithmetic(a, b, operator){
+  switch(operator) {
+      case 'add':
+      return a + b;
+      case 'subtract':
+      return a - b;
+      case 'multiply':
+      return a * b;
+      case 'divide' :
+      return a /b;
+  }
+}
+
